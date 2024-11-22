2ebfc45d40c1204e3d65880843652196dd396910
vbonnard <dev@MacBook-Pro-3.local>
2024-11-22 16:17:22 +0100

    upd main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
-#include ""
+#include "include/structs.h"
 
+t_app	App;
 
-int main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
-	return 0;
-}
+	t_user_data	player1;
+	t_user_data	player2;
 
+	init_app();
+	while (App.running)
+	{
+		menu_window(&player1, &player2);
+		game_window(player1, player2);
+	}
+	kill_app();
+	return (0);
+}
