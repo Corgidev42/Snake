5b039e77ff0601fdd1dee5c5801ac2053896e76a
vbonnard <dev@MacBook-Pro-3.local>
2024-11-23 15:29:53 +0100

    upd menu.h

--- include/menu.h
+++ include/menu.h
@@ -17,22 +17,13 @@ void	menu_window(t_user_data *player1, t_user_data *player2);
  * @param player1 Pointeur vers les données du joueur 1.
  * @param player2 Pointeur vers les données du joueur 2.
  */
-void	do_input(t_user_data *player1, t_user_data *player2);
+void	do_input(t_user_data *player1, t_user_data *player);
 
 /**
  * @brief Affiche le choix du joueur 1 dans le menu.
  *
  * @param player1 Données du joueur 1.
- * @param rect_left Rectangle de la zone d'affichage pour le joueur 1.
+ * @param rect_left Rectangle de la zone d'affichage pour le joueur.
  */
-void	render_player_choice(t_user_data player1, SDL_Rect rect_left);
-
-/**
- * @brief Affiche le choix du joueur 2 dans le menu.
- *
- * @param player2 Données du joueur 2.
- * @param rect_right Rectangle de la zone d'affichage pour le joueur 2.
- */
-void	render_player_choice(t_user_data player2, SDL_Rect rect_right);
 
 #endif
